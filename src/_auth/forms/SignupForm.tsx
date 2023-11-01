6506b61e1e82aa2b18f3ef6332eb4ae8e1bf7d77
Dejvi <dejviepadejvi@gmail.com>
2023-11-01 12:15:07 +0100

    punuam tek auth

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -16,8 +16,9 @@ import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation"
 import Loader from "@/components/shared/Loader"
 import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
 
-const SignupForm = () => {
+const SignupForm =  () => {
  const isLoading = false;
 
   // 1. Define your form.
@@ -32,10 +33,9 @@ const SignupForm = () => {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+ async function onSubmit(values: z.infer<typeof SignupValidation>) {
+   const newUser = await createUserAccount(values)
+   console.log(newUser)
   }
 
   return (
