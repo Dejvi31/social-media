75743be5c3ebada874ec6809fc8b66cdd6f3d07e
Dejvi <dejviepadejvi@gmail.com>
2023-11-06 13:43:23 +0100

    filluam pak me react query me creatingAccount dhe signInAccount

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -9,13 +9,15 @@ import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation"
 import Loader from "@/components/shared/Loader"
 import { Link } from "react-router-dom"
-import { createUserAccount } from "@/lib/appwrite/api"
 import { useToast } from "@/components/ui/use-toast"
+import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
 
 const SignupForm =  () => {
   const {toast} = useToast()
- const isLoading = false;
 
+  const {mutateAsync: createUserAccount, isLoading: isCreatingUser} = useCreateUserAccount()
+
+  const {mutateAsync: signInAccount, isLoading: isSigningIn} = useSignInAccount()
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -35,7 +37,14 @@ const SignupForm =  () => {
     return toast({title: 'Sign up failed. Please try again.'})
   }
 
-  // const session = await signInAccount()
+  const session = await signInAccount({
+    email: values.email,
+    password:values.password,
+  })
+
+  if(!session) {
+    return toast({title: 'Sign in failed. Please try again.'})
+  }
 
   }
 
@@ -102,7 +111,7 @@ const SignupForm =  () => {
           )}
         />
         <Button type="submit" className="shad-button_primary">
-          {isLoading ? (
+          {isCreatingUser? (
             <div className="flex-center gap-2"><Loader />Loading...</div>
           ): "Sign Up"}
         </Button>
