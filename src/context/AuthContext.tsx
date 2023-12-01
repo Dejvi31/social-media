5f43cba407e49e7465a096840f48dcd908d8f938
Dejvi <dejviepadejvi@gmail.com>
2023-12-01 14:01:37 +0100

    rregulluam SignupForm

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -67,14 +67,10 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   };
 
   useEffect(() => {
-    const cookieFallback = localStorage.getItem("cookieFallback");
-    if (
-      cookieFallback === "[]" ||
-      cookieFallback === null ||
-      cookieFallback === undefined
-    ) {
-      navigate("/sign-in");
-    }
+    if(
+      localStorage.getItem('cookeFallback') === '[]'
+    ) navigate('/sign-in')
+    
 
     checkAuthUser();
   }, []);
